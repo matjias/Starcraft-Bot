61db5959bddf9b3bde44126b9a1f86fb9574b468
MildanWorld <stefano_boy_95@hotmail.com>
2016-02-17 18:27:19 +0100

    Added a worker count that counts up whenever it makes a new worker and counts down when one is destroyed

--- ExampleAIModule/Source/ExampleAIModule.cpp
+++ ExampleAIModule/Source/ExampleAIModule.cpp
@@ -4,9 +4,14 @@
 using namespace BWAPI;
 using namespace Filter;
 
+int workerCount;
+
 void ExampleAIModule::onStart() {
 	Broodwar->enableFlag(Flag::UserInput);
 	Broodwar->setCommandOptimizationLevel(2);
+
+
+
 }
 
 void ExampleAIModule::onEnd(bool isWinner) {
@@ -14,6 +19,10 @@ void ExampleAIModule::onEnd(bool isWinner) {
 }
 
 void ExampleAIModule::onFrame() {
+	Broodwar->drawTextScreen(200, 0, "FPS: %d", Broodwar->getFPS());
+	Broodwar->drawTextScreen(200, 20, "Average FPS: %f", Broodwar->getAverageFPS());
+	Broodwar->drawTextScreen(200, 40, "Worker Count: %d", workerCount);
+	
 	// AI Logic goes here
 
 }
@@ -36,6 +45,9 @@ void ExampleAIModule::onNukeDetect(BWAPI::Position target) {
 }
 
 void ExampleAIModule::onUnitDiscover(BWAPI::Unit unit) {
+	if (unit->getType().isWorker() && unit->getPlayer() == Broodwar->self()) {
+		workerCount++;
+	}
 }
 
 void ExampleAIModule::onUnitEvade(BWAPI::Unit unit) {
@@ -52,6 +64,11 @@ void ExampleAIModule::onUnitCreate(BWAPI::Unit unit) {
 }
 
 void ExampleAIModule::onUnitDestroy(BWAPI::Unit unit) {
+	if (unit->getType().isWorker() && unit->getPlayer() == Broodwar->self()) {
+		workerCount--;
+	}
+
+	
 }
 
 void ExampleAIModule::onUnitMorph(BWAPI::Unit unit) {
