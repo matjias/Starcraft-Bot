e50cda41b8bd6d7513da58d7afbd70906064dad0
MildanWorld <stefano_boy_95@hotmail.com>
2016-02-18 23:09:19 +0100

    Now creates pylons when psi is low and it has enough minerals for it

--- ExampleAIModule/Source/ExampleAIModule.cpp
+++ ExampleAIModule/Source/ExampleAIModule.cpp
@@ -4,6 +4,7 @@
 using namespace BWAPI;
 using namespace Filter;
 
+int availableSupply;
 int workerCount;
 
 void ExampleAIModule::onStart() {
@@ -18,16 +19,27 @@ void ExampleAIModule::onEnd(bool isWinner) {
 }
 
 void ExampleAIModule::onFrame() {
+	availableSupply = (Broodwar->self()->supplyTotal() - Broodwar->self()->supplyUsed()) / 2;
+
 	Broodwar->drawTextScreen(200, 0, "FPS: %d", Broodwar->getFPS());
-	Broodwar->drawTextScreen(200, 20, "Average FPS: %f", Broodwar->getAverageFPS());
-	Broodwar->drawTextScreen(200, 40, "Unit Count: %d", Broodwar->self()->supplyUsed()/2);
-	Broodwar->drawTextScreen(200, 60, "Worker Count: %d", workerCount);
+	//Broodwar->drawTextScreen(200, 20, "Average FPS: %f", Broodwar->getAverageFPS());
+	Broodwar->drawTextScreen(200, 20, "Available Supply: %d", availableSupply);
+	//Broodwar->drawTextScreen(200, 60, "Worker Count: %d", workerCount);
 	
 	// AI Logic goes here
 
-	// Mining minerals
+	// All Units loop
 	for (auto &u : Broodwar->self()->getUnits()) {
+		// If unit does not exist or is still being made, we ignore it
+		if (!u->exists() || !u->isCompleted()) {
+			continue;
+		}
+
+
+		// Worker logic
+		// Currently only mining minerals
 		if (u->getType().isWorker()) {
+			// Gotta mine them bitcoins
 			if (u->isIdle()) {
 				if (u->isCarryingMinerals()) {
 					u->returnCargo();
@@ -35,7 +47,58 @@ void ExampleAIModule::onFrame() {
 					u->gather(u->getClosestUnit(IsMineralField));
 				}
 			}
+
+
+			
+
+			
 		}
+
+		// Resource Depot (central stuff or something)
+		else if (u->getType().isResourceDepot()) {
+			if (u->isIdle() && Broodwar->self()->minerals() >= 50 && 
+				(availableSupply > 1 || Broodwar->self()->incompleteUnitCount(u->getType().getRace().getSupplyProvider()) > 0)) {
+				u->train(u->getType().getRace().getWorker());
+			}
+
+			// Supply is getting low.. Might need another Pylon now
+			if (availableSupply <= 1 && Broodwar->self()->minerals() >= 100) {
+				UnitType pylon = u->getType().getRace().getSupplyProvider();
+				static int lastCheck = 0;
+
+				if (lastCheck + 400 < Broodwar->getFrameCount() &&
+					Broodwar->self()->incompleteUnitCount(pylon) == 0) {
+
+					lastCheck = Broodwar->getFrameCount();
+
+					Unit supplyBuilder = u->getClosestUnit(GetType == pylon.whatBuilds().first &&
+						(IsIdle || IsGatheringMinerals) && IsOwned);
+
+					if (supplyBuilder) {
+						TilePosition buildLocation = Broodwar->getBuildLocation(pylon, supplyBuilder->getTilePosition());
+
+						if (buildLocation) {
+
+							// Box to display where we make the pylon
+							Broodwar->registerEvent([buildLocation, pylon](Game*) {
+								Broodwar->drawBoxMap(Position(buildLocation),
+								Position(buildLocation + pylon.tileSize()),
+								Colors::Blue);
+								}, nullptr,
+								pylon.buildTime() + 100);
+
+							// Making the pylon
+							supplyBuilder->build(pylon, buildLocation);
+						}
+					}
+
+					
+				}
+			}
+		}
+
+
+
 	}
 }
 
