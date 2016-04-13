2f09088cbfce7d1069be41e4ae8d2ab847a9ebe6
matjias <Mathiassixped.dk>
2016-04-13 16:50:37 +0200

    Har ikke testet dette martin

--- ExampleAIModule/Source/Army.h
+++ ExampleAIModule/Source/Army.h
@@ -12,7 +12,7 @@ public:
 	~Army();
 
 	void _init();
-
+	void Army::attack(int atNum);
 	void update(Scouting scoutClass);
 	bool buildZealot(BWAPI::Unit u);
 	bool buildDragoon(BWAPI::Unit u);
@@ -22,12 +22,14 @@ public:
 	void zealotRush();
 	void setAnalyzed(bool analyzed);
 	int moved, countAtEnemyChoke;
-	bool Army::zealotAtPos(BWAPI::Unit zealot, BWAPI::TilePosition pos);
+	bool Army::unitAtPos(BWAPI::Unit zealot, BWAPI::TilePosition pos);
+	bool Army::squadAtPos(Unitset uSet, TilePosition pos);
 	std::vector<BWAPI::Unit> getZealots();
 	BWAPI::Position enemyChoke;
 	void enemyBaseDestroyed();
-	void Army::addSquadMember(int squadNumber, BWAPI::Unit u);
+	void Army::addSquadMember(BWAPI::Unit u);
 	void Army::buildSquad(std::vector<std::pair<BWAPI::UnitType, int>> list);
+	void Army::combat();
 private:
 	void attack();
 };
