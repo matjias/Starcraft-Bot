2df3aba851695b318ba2bedccbeadd1660dbdd6e
MDariush <dariush@live.dk>
2016-06-03 09:20:32 +0200

    Merge

--- ExampleAIModule/Source/ResourceSpender.h
+++ ExampleAIModule/Source/ResourceSpender.h
@@ -1,13 +1,14 @@
 #pragma once
-
+#include <BWAPI.h>
 
 class ResourceSpender {
 public:
 	ResourceSpender();
 	~ResourceSpender();
 
-
-
 private:
-
+	void addInvestment(BWAPI::UnitType unitType, bool urgent);
+	void addNeededTech(int priority);
+	void workerNeeded();
+	void supplyNeeded();
 };
